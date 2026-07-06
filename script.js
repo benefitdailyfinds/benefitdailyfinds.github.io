19f299f09ef7a0071d3b4f5f6c3b86a034af675a
benefitdailyfinds <miracle92star@gmail.com>
2026-07-06 22:11:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const products = [
   {
     title: "360° Cable Organizer",
     category: "Smart Tech",
-    image: image: "images/cable organizer.jpg",
+    image: image: "images/cable organizer.jpeg",
     description: "Keep your cables neat and organized.",
     link: link: "https://link.amazon/B0aAGTv3w"
   },
